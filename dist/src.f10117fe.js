752f6ec0f1b3083a3d54d0237384753d33a3c754
noomdalv <vladlunasan@gmail.com>
2020-08-09 05:43:25 -0500

    index.ts now renders google maps api

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -85172,6 +85172,13 @@ var user = new User_1.User();
 var company = new Company_1.Company();
 console.log(user);
 console.log(company);
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 2,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
 },{"./User":"src/User.ts","./Company":"src/Company.ts"}],"C:/Users/striu/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -85200,7 +85207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61881" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62120" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,11 @@ const company = new Company();
 
 console.log(user);
 console.log(company);
+
+new google.maps.Map(document.getElementById("map"), {
+	zoom: 2,
+	center: {
+		lat: 0,
+		lng: 0,
+	},
+});
